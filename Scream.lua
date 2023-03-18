ea36b7f87d863f6376a1c80ee2b9f8ea6ca02bad
andrewcodelua <112720895+andrewcodelua@users.noreply.github.com>
2023-03-18 17:58:31 -0300

    Update Scream.lua

--- Scream.lua
+++ Scream.lua
@@ -1,28 +1,56 @@
---this is monster
- wait(1)
-    local Creator = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors%20Entity%20Spawner/Source.lua"))()
-
-    local entity = Creator.createEntity({
-        Model = "https://github.com/andrewcodelua/Random-Things-That-I-Do/blob/main/Scream.rbxm?raw=true",
-        Speed = 400,
-        DelayTime = 2,
-        HeightOffset = 0,
-        CanKill = true,
-        BreakLights = true,
-        FlickerLights = {
-            true,
-            1,
+local Creator = loadstring(game:HttpGet("https://pastebin.com/raw/abu9Z5Rw"))()
+ 
+-- Create entity
+local entity = Creator.createEntity({
+    CustomName = "Scream", -- Custom name of your entity
+    Model = "https://github.com/andrewcodelua/Random-Things-That-I-Do/blob/main/Scream.rbxm?raw=true", -- Can be GitHub file or rbxassetid
+    Speed = 500, -- Percentage, 100 = default Rush speed
+    DelayTime = 5, -- Time before starting cycles (seconds)
+    HeightOffset = 0,
+    CanKill = true,
+    KillRange = 50,
+    BreakLights = true,
+    BackwardsMovement = true,
+    FlickerLights = {
+        true, -- Enabled/Disabled
+        3, -- Time (seconds)
+    },
+    Cycles = {
+        Min = 1,
+        Max = 4,
+        WaitTime = 0.2,
+    },
+    CamShake = {
+        true, -- Enabled/Disabled
+        {8, 100, 0.3, 2}, -- Shake values (don't change if you don't know)
+        100, -- Shake start distance (from Entity to you)
+    },
+    Jumpscare = {
+        true, -- Enabled/Disabled
+        {
+            Image1 = "rbxassetid://12827393772", -- Image1 url
+            Image2 = "rbxassetid://12827393772", -- Image2 url
+            Shake = true,
+            Sound1 = {
+                9113073742, -- SoundId
+                { Volume = 1 }, -- Sound properties
+            },
+            Sound2 = {
+                10483837590, -- SoundId
+                { Volume = 1 }, -- Sound properties
+            },
+            Flashing = {
+                true, -- Enabled/Disabled
+                Color3.fromRGB(255, 0, 0), -- Color
+            },
+            Tease = {
+                true, -- Enabled/Disabled
+                Min = 1,
+                Max = 3,
+            },
         },
-        Cycles = {
-            Min = 2,
-            Max = 2,
-            WaitTime = 2,
-        },
-        CamShake = {
-            true,
-            {5, 15, 0.1, 1},
-            100,
-        },
-    })
-
-    Creator.runEntity(entity)
+    },
+    CustomDialog = {"What... The heck just happened?", "There is no such 'Scream'", "Wow, He is very fast", "I think that you must use what you learned from rush."}, -- Custom death message
+})
+-- Run the created entity
+Creator.runEntity(entity)
