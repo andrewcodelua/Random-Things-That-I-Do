0ec5bec6419ca3dacef457d438c4184231800b50
andrewcodelua <112720895+andrewcodelua@users.noreply.github.com>
2023-03-18 22:58:41 -0300

    Update ManiacMode.lua

--- ManiacMode.lua
+++ ManiacMode.lua
@@ -1,3 +1,11 @@
 firesignal(game:GetService("ReplicatedStorage").EntityInfo.Caption.OnClientEvent, 'Maniac Mode activated.',true,2)
 wait(1)
 firesignal(game:GetService("ReplicatedStorage").EntityInfo.Caption.OnClientEvent, 'hope you were prepared',true,1)
+wait(1)
+firesignal(game:GetService("ReplicatedStorage").EntityInfo.Caption.OnClientEvent, 'btw entities spawn every 2 min',true,2)
+while true do
+  wait(120)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Scream.lua"))()
+  wait(120)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Bounce.lua"))()
+end
