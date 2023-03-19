4662d0b4d313a220b905df30c661b715b3bb8da1
andrewcodelua <112720895+andrewcodelua@users.noreply.github.com>
2023-03-18 22:45:27 -0300

    Create ManiacMode.lua

---
+++ ManiacMode.lua
@@ -0,0 +1,3 @@
+firesignal(game:GetService("ReplicatedStorage").EntityInfo.Caption.OnClientEvent, 'Maniac Mode activated.',true,2)
+wait(1)
+firesignal(game:GetService("ReplicatedStorage").EntityInfo.Caption.OnClientEvent, 'hope you were prepared',true,1)
