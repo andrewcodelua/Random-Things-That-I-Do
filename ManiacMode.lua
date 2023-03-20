b8f4371535250454420316a20c91eac484e9d4f5
andrewcodelua <112720895+andrewcodelua@users.noreply.github.com>
2023-03-20 19:24:05 -0300

    Update ManiacMode.lua

--- ManiacMode.lua
+++ ManiacMode.lua
@@ -1,11 +1,6 @@
+print("activated! entities spawn every 2 min")
 wait(1.2)
-game.StarterGui:SetCore("SendNotification", {
-Title = "Maniac Mode activated"; -- the title (ofc)
-Text = "btw entities spawn every 2 minutes k?"; -- what the text says (ofc)
-Icon = "rbxassetid://57254792"; -- the image if u want. 
-Duration = 5; -- how long the notification should in secounds
-})
-m:Destroy()
+
 while true do
   wait(120)
     loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Scream.lua"))()
