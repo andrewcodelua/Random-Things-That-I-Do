aa89df043c03d8c39ebbc0be63a083de0ae47753
andrewcodelua <112720895+andrewcodelua@users.noreply.github.com>
2023-04-01 20:34:04 -0300

    Create Intruder.lua

---
+++ Intruder.lua
@@ -0,0 +1,27 @@
+wait(1)
+    local Creator = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors%20Entity%20Spawner/Source.lua"))()
+
+    local entity = Creator.createEntity({
+        Model = "https://github.com/andrewcodelua/Random-Things-That-I-Do/blob/main/Intruder.rbxm?raw=true",
+        Speed = 400,
+        DelayTime = 2,
+        HeightOffset = 0,
+        CanKill = true,
+        BreakLights = true,
+        FlickerLights = {
+            true,
+            1,
+        },
+        Cycles = {
+            Min = 4,
+            Max = 10,
+            WaitTime = 2,
+        },
+        CamShake = {
+            true,
+            {5, 15, 0.1, 1},
+            100,
+        },
+    })
+
+    Creator.runEntity(entity)
