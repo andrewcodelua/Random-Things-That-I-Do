f644c87428f0ecfcd05b83449010fd15b84a14b4
andrewcodelua <112720895+andrewcodelua@users.noreply.github.com>
2023-04-06 20:18:34 -0300

    Update IntruderMode.lua

--- IntruderMode.lua
+++ IntruderMode.lua
@@ -1,5 +1,10 @@
     loadstring(game:HttpGet("https://pastebin.com/raw/8SmKpVGF"))()
 wait(120)
 while true do
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
       loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+    wait(120)
 end
