534fedd09d7991c9ba716e1981ba2659e849cb4a
andrewcodelua <112720895+andrewcodelua@users.noreply.github.com>
2023-04-07 13:28:32 -0300

    Update IntruderMode.lua

--- IntruderMode.lua
+++ IntruderMode.lua
@@ -1,14 +1,292 @@
     loadstring(game:HttpGet("https://pastebin.com/raw/8SmKpVGF"))()
+local Achievements = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))()
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Call Incoming", -- Title
+	Text = "This is a automated message.", -- Description
+})
+wait(2)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Call Incoming", -- Title
+	Text = "This means your shift has begun. Explore the hotel until the call service is back.", -- Description
+})
+wait(5)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Call Incoming", -- Title
+	Text = "When its back, you will be rescued.", -- Description
+})
+wait(2)
  game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Warning!", -- Title
-	Text = "Someone was spotted outside (succesfully loaded!)", -- Description
+	Text = "Someone was spotted outside!", -- Description
+})
+wait(2)
+Achievements.Get({
+    Title = "This hotel is scary..",
+    Desc = "WHAT WAS THAT SOUND?!",
+    Reason = 'Begin your shift',
+    Image = "https://tr.rbxcdn.com/ae32585e84a2b1762ef3de2fca587c22/150/150/Image/Png",
 })
 wait(120)
-while true do
+
     game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Warning!", -- Title
 	Text = "Someone was spotted in camera 9!", -- Description
 })
       loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
-    wait(120)
-end
+Achievements.Get({
+    Title = "Nasty Shapeshifter",
+    Desc = "What do you want from me?",
+    Reason = 'Find the Intruder',
+    Image = "https://tr.rbxcdn.com/674f6af4fd48c98c439937fc8f309880/150/150/Image/Png",
+})
+
+wait(120)
+Achievements.Get({
+    Title = "2 More Minutes",
+    Desc = "It isnt over so fastly!",
+    Reason = 'Wait 2 minutes after the intruder first came',
+    Image = "https://tr.rbxcdn.com/cd0f03ad6afc93dff764c4bf8c4dfa5d/420/420/Image/Png",
+})
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Call Incoming", -- Title
+	Text = "Is anyone there?", -- Description
+})
+
+wait(2)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Call Incoming", -- Title
+	Text = "The call service is back.", -- Description
+})
+wait(2)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Call Incoming", -- Title
+	Text = "Your shift is over.", -- Description
+})
+wait(2)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Call Incoming", -- Title
+	Text = "You will be rescued in about 6 minutes. Get alive!", -- Description
+})
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Title
+	Text = "Someone was spotted in camera 9!", -- Description
+})
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/andrewcodelua/Random-Things-That-I-Do/main/Intruder.lua"))()
+wait(120)
+Achievements.Get({
+    Title = "Over..?",
+    Desc = "What will happen next?",
+    Reason = 'Be rescued',
+    Image = "https://i.ytimg.com/vi/HM6nQZ0r9ho/maxresdefault.jpg",
+})
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Call Incoming", -- Title
+	Text = "The police is here to rescue you.", -- Description
+})
+wait(2)
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Call Incoming", -- Title
+	Text = "Come outside!", -- Description
+})
+wait(5)
+Achievements.Get({
+    Title = "Over..?",
+    Desc = "What will happen next?",
+    Reason = 'Be rescued',
+    Image = "https://i.ytimg.com/vi/HM6nQZ0r9ho/maxresdefault.jpg",
+})
+wait(5)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/rouxhaver/scripts-2/main/KickFC.lua"))()
+wait(0.1)
+game.Players.LocalPlayer:Kick("You got rescued! If you wish to spectate, press Freecam (PC only)")
+
